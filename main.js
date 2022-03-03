f7505a65b7ded22e48c7149f070139f7a7db5833
Dharmik <hingudharmik4@gmail.com>
2022-03-03 18:58:59 +0530

    make essential changes

---
+++ main.js
@@ -0,0 +1,44 @@
+const btnCaculate = document.getElementById('calculate')
+
+btnCaculate.addEventListener('click', () => {
+	countLines()
+	countWords()
+	countcharacter()
+})
+
+function countWords() {
+	var text = document.getElementById('inputField').value
+	var numWords = 0
+	for (var i = 0; i < text.length; i++) {
+		var currentCharacter = text[i]
+		if (currentCharacter == ' ') {
+			numWords += 1
+		}
+	}
+	numWords += 1
+	document.getElementById('show').innerHTML = numWords
+}
+function countcharacter() {
+	var text = document.getElementById('inputField').value
+	var numchar = 0
+	var char = 0
+	for (var i = 0; i < text.length; i++) {
+		var character = text[i]
+		if (text[i] != ' ') {
+			char++
+		}
+	}
+	document.getElementById('show2').innerHTML = char
+}
+function countLines() {
+	var text = document.getElementById('inputField').value
+	var lines = 0
+	for (var i = 0; i < text.length; i++) {
+		var char = text[i]
+		if (text[i] == '\n') {
+			lines++
+		}
+	}
+	lines += 1
+	document.getElementById('show3').innerHTML = lines
+}
